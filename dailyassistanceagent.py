535cc654b429e1a6e833ac8146ba869c8cb483ce
RAH333 <142141323+RAH333@users.noreply.github.com>
2025-11-30 17:26:16 +0530

    Update dailyassistanceagent.py

--- dailyassistanceagent.py
+++ dailyassistanceagent.py
@@ -1,1 +1,7 @@
-
+#!export GOOGLE_GENAI_USE_VERTEXAI=FALSE && adk create daily-assistant-agent-model --model gemini-2.5-flash-lite --api_key GOOGLE_API_KEY
+!export GOOGLE_GENAI_USE_VERTEXAI=FALSE && adk create daily-assistant-agent-model --model gemini-2.5-flash-lite --api_key 
+#!export GOOGLE_GENAI_USE_VERTEXAI=FALSE && adk create daily-assistant-agent-model --model gemini-2.5-flash-lite --api_key=$GOOGLE_API_KEY
+#!export GOOGLE_GENAI_USE_VERTEXAI=FALSE && adk create daily-assistant-agent-model --model gemini-2.5-flash-lite --api_key=$
+#!adk create daily-assistant-agent --model gemini-2.5-flash-lite --api_key=$GOOGLE_API_KEY
+#!cd daily-assistant-agent-model
+%%writefile daily-assistant-agent-model/agent.py
